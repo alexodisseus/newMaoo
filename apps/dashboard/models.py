10630f73e6e0cd9452f9e99195d5e01d79b3c886
alexodisseus <alexodisseus@gmail.com>
2024-08-24 02:01:07 -0300

    first commit

---
+++ apps/dashboard/models.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from flask_login import UserMixin
+
+from apps import db, login_manager
+
+from apps.authentication.util import hash_pass
+from apps.authentication.models import Users
+
+
+
+
+
+@login_manager.user_loader
+def user_loader(id):
+    return Users.query.filter_by(id=id).first()
+
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    user = Users.query.filter_by(username=username).first()
+    return user if user else None
+
+
+
+
+def load_users():
+    asd = Users.query.all()    
+
+    return asd
